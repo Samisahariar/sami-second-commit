df36be62355a212f489764d85e9857922bc87056
Samisahariar <samisahariar69@gmail.com>
2024-02-15 03:40:24 -0800

    almost done

--- styles/dom.js
+++ styles/dom.js
@@ -2,6 +2,11 @@ const forgotpasselement = document.querySelector('#forgotpass')
 const loginpage = document.querySelector('#login-page')
 const forgotpage = document.querySelector('#forgot-page')
 const returnhomepagebutton = document.querySelector('#return-to-login-page')
+const loginmainpage = document.querySelector('#loginmainpage')
+const mainpage = document.querySelector('#main-page')
+const loginfrontpage = document.querySelector('#login-button')
+const userinput = document.querySelector('#user-input')
+const passwordinput = document.querySelector('#password-input') 
 
 
 function changelayout(frompage, topage){
@@ -14,4 +19,13 @@ forgotpasselement.addEventListener('click', function(){
 })
 returnhomepagebutton.addEventListener('click', function(){
     changelayout(forgotpage, loginpage)
+})
+loginmainpage.addEventListener('click', function(){
+    changelayout(mainpage, loginpage)
+})
+
+loginfrontpage.addEventListener('click', function(){
+    if(userinput.value === "sami" && passwordinput.value === "sami1212"){
+        changelayout(mainpage, loginpage)
+    }
 })
